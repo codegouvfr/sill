484dad3b908d0c10d9984f6d1fd1006865f6334f
Jérôme Burkard <22095555+JeromeBu@users.noreply.github.com>
2024-03-20 12:26:26 +0100

    make it possible to use root .env with FLAT env variables

--- api/src/env.ts
+++ api/src/env.ts
@@ -1,11 +1,8 @@
-import { symToStr } from "tsafe/symToStr";
-import { assert } from "tsafe/assert";
 import * as JSONC from "comment-json";
 import { z } from "zod";
 import { zLocalizedString } from "./core/ports/GetSoftwareExternalData";
-import { is } from "tsafe/is";
 
-const zParsedCONFIGURATION = z.object({
+const zConfiguration = z.object({
     "keycloakParams": z
         .object({
             "url": z.string().nonempty(), //Example: https://auth.code.gouv.fr/auth (with the /auth at the end)
@@ -33,42 +30,59 @@ const zParsedCONFIGURATION = z.object({
     // we use the GitHub API for pre filling the version when adding a software
     "githubPersonalAccessTokenForApiRateLimit": z.string().nonempty(),
     //Port we listen to, default 8080
-    "port": z.number().optional(),
+    "port": z.coerce.number().optional(),
     "isDevEnvironnement": z.boolean().optional(),
     // Completely disable this instance and redirect to another url
     "redirectUrl": z.string().optional(),
     "externalSoftwareDataOrigin": z.enum(["wikidata", "HAL"]).optional()
 });
 
-const { parsedCONFIGURATION } = (() => {
+const getJsonConfiguration = () => {
     const { CONFIGURATION } = process.env;
-
-    if (CONFIGURATION === undefined) {
-        throw new Error(
-            `We need a ${symToStr({
-                CONFIGURATION
-            })} environnement variable`
-        );
+    if (CONFIGURATION) {
+        try {
+            return JSONC.parse(CONFIGURATION) as any;
+        } catch (error) {
+            throw new Error(
+                `The CONFIGURATION environnement variable is not a valid JSONC string (JSONC = JSON + Comment support)\n${CONFIGURATION}: ${String(
+                    error
+                )}`
+            );
+        }
     }
 
-    let parsedCONFIGURATION: unknown;
+    return {
+        "keycloakParams": {
+            "url": process.env.SILL_KEYCLOAK_URL,
+            "realm": process.env.SILL_KEYCLOAK_REALM,
+            "clientId": process.env.SILL_KEYCLOAK_CLIENT_ID,
+            "adminPassword": process.env.SILL_KEYCLOAK_ADMIN_PASSWORD,
+            "organizationUserProfileAttributeName": process.env.SILL_KEYCLOAK_ORGANIZATION_USER_PROFILE_ATTRIBUTE_NAME
+        },
+        "readmeUrl": process.env.SILL_README_URL,
+        "termsOfServiceUrl": process.env.SILL_TERMS_OF_SERVICE_URL,
+        "jwtClaimByUserKey": {
+            "id": process.env.SILL_JWT_ID,
+            "email": process.env.SILL_JWT_EMAIL,
+            "organization": process.env.SILL_JWT_ORGANIZATION
+        },
+        "dataRepoSshUrl": process.env.SILL_DATA_REPO_SSH_URL,
+        "sshPrivateKeyForGitName": process.env.SILL_SSH_NAME,
+        "sshPrivateKeyForGit": process.env.SILL_SSH_PRIVATE_KEY,
+        "githubPersonalAccessTokenForApiRateLimit": process.env.SILL_GITHUB_TOKEN,
+        "githubWebhookSecret": process.env.SILL_WEBHOOK_SECRET,
+        "port": parseInt(process.env.SILL_API_PORT ?? ""),
+        "isDevEnvironnement": process.env.SILL_IS_DEV_ENVIRONNEMENT?.toLowerCase() === "true",
+        "externalSoftwareDataOrigin": process.env.SILL_EXTERNAL_SOFTWARE_DATA_ORIGIN
+    };
+};
 
-    try {
-        parsedCONFIGURATION = JSONC.parse(CONFIGURATION) as any;
-    } catch (error) {
-        throw new Error(
-            `The CONFIGURATION environnement variable is not a valid JSONC string (JSONC = JSON + Comment support)\n${CONFIGURATION}: ${String(
-                error
-            )}`
-        );
-    }
+const getValidConfiguration = (): z.infer<typeof zConfiguration> => {
+    const configuration = getJsonConfiguration();
+    return zConfiguration.parse(configuration);
+};
 
-    zParsedCONFIGURATION.parse(parsedCONFIGURATION);
-
-    assert(is<ReturnType<(typeof zParsedCONFIGURATION)["parse"]>>(parsedCONFIGURATION));
-
-    return { parsedCONFIGURATION };
-})();
+const parsedCONFIGURATION = getValidConfiguration();
 
 export const env = {
     ...parsedCONFIGURATION,
