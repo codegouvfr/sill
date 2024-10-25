b3f0935b26beb699721fe72cf4372b82f0c83b76
Jérôme Burkard <22095555+JeromeBu@users.noreply.github.com>
2024-10-25 10:56:49 +0200

    show private instances to connected users

--- web/src/ui/pages/softwareDetails/ReferencedInstancesTab.tsx
+++ web/src/ui/pages/softwareDetails/ReferencedInstancesTab.tsx
@@ -1,6 +1,6 @@
 import { declareComponentKeys } from "i18nifty";
 import { useTranslation } from "ui/i18n";
-import { fr } from "@codegouvfr/react-dsfr";
+import { fr, type FrCxArg } from "@codegouvfr/react-dsfr";
 import { tss } from "tss-react";
 import { Equals } from "tsafe";
 import { assert } from "tsafe/assert";
@@ -11,51 +11,104 @@ import type { Link } from "type-route";
 import { useCore } from "../../../core";
 import { routes } from "../../routes";
 
+type InstanceInList = {
+    id: number;
+    organization: string;
+    instanceUrl: string | undefined;
+    targetAudience: string;
+    isPublic: boolean;
+};
+
 export type Props = {
     className?: string;
-    instanceList: {
-        id: number;
-        organization: string;
-        instanceUrl: string;
-        targetAudience: string;
-    }[];
+    instanceList: InstanceInList[];
     createInstanceLink: Link;
+};
+
+type ReferenceInstancesSectionProps = {
+    classname?: FrCxArg;
+    instances: InstanceInList[];
+    visibility: "public" | "private";
 };
 
 export const ReferencedInstancesTab = (props: Props) => {
     const { className, instanceList, createInstanceLink, ...rest } = props;
-    const { userAuthentication } = useCore().functions;
-    const isUserLoggedIn = userAuthentication.getIsUserLoggedIn();
-
     /** Assert to make sure all props are deconstructed */
     assert<Equals<typeof rest, {}>>();
 
     const { t } = useTranslation({ ReferencedInstancesTab });
-    const { classes, cx } = useStyles();
 
-    const groupInstanceByOrganization = groupBy(instanceList, "organization");
+    const publicInstances = instanceList.filter(instance => instance.isPublic);
+    const privateInstances = instanceList.filter(instance => !instance.isPublic);
 
     return (
         <section className={className}>
-            <p className={fr.cx("fr-text--bold")}>
-                {t("instanceCount", {
-                    "instanceCount": instanceList.length,
-                    "publicOrganizationCount": Object.keys(groupInstanceByOrganization)
-                        .length
-                })}
+            <ReferenceInstancesSection instances={publicInstances} visibility="public" />
+            <ReferenceInstancesSection
+                instances={privateInstances}
+                visibility="private"
+                classname={"fr-mt-4w"}
+            />
+            <Button className={fr.cx("fr-mt-3w")} linkProps={createInstanceLink}>
+                {t("add instance")}
+            </Button>
+        </section>
+    );
+};
+
+const ReferenceInstancesSection = ({
+    instances,
+    visibility,
+    classname
+}: ReferenceInstancesSectionProps) => {
+    const { userAuthentication } = useCore().functions;
+    const isUserLoggedIn = userAuthentication.getIsUserLoggedIn();
+    const { classes, cx } = useStyles();
+    const { t } = useTranslation({ ReferencedInstancesTab });
+
+    if (instances.length === 0) return null;
+
+    const instancesByOrganisation = groupBy(instances, "organization");
+
+    if (visibility === "private" && !isUserLoggedIn) {
+        return (
+            <>
+                <p className={fr.cx("fr-text--bold", classname)}>
+                    {t("privateInstanceCount", {
+                        "instanceCount": instances.length,
+                        "organizationCount": Object.keys(instancesByOrganisation).length
+                    })}
+                </p>
+                <span className={fr.cx("fr-hint-text")}>
+                    {t("connect to see private instances")}
+                </span>
+            </>
+        );
+    }
+
+    return (
+        <>
+            <p className={fr.cx("fr-text--bold", classname)}>
+                {t(
+                    visibility === "public"
+                        ? "publicInstanceCount"
+                        : "privateInstanceCount",
+                    {
+                        "instanceCount": instances.length,
+                        "organizationCount": Object.keys(instancesByOrganisation).length
+                    }
+                )}
             </p>
-            {Object.keys(groupInstanceByOrganization).map(organization => {
-                const groupedInstanceList = instanceList.filter(
-                    instance => instance.organization === organization
-                );
+            {Object.keys(instancesByOrganisation).map(organization => {
+                const instancesForOrganization = instancesByOrganisation[organization];
 
                 return (
                     <Accordion
                         key={organization}
-                        label={`${organization} (${groupedInstanceList.length})`}
+                        label={`${organization} (${instancesForOrganization.length})`}
                     >
                         <div className={classes.accordionGrid}>
-                            {groupedInstanceList.map(instance => {
+                            {instancesForOrganization.map(instance => {
                                 const { instanceUrl, targetAudience } = instance;
                                 return (
                                     <div
@@ -97,16 +150,21 @@ export const ReferencedInstancesTab = (props: Props) => {
                                                 </Button>
                                             )}
 
-                                            <a
-                                                className={cx(
-                                                    fr.cx("fr-btn", "fr-btn--secondary")
-                                                )}
-                                                href={instanceUrl}
-                                                target="_blank"
-                                                rel="noreferrer"
-                                            >
-                                                {t("go to instance")}
-                                            </a>
+                                            {instanceUrl && (
+                                                <a
+                                                    className={cx(
+                                                        fr.cx(
+                                                            "fr-btn",
+                                                            "fr-btn--secondary"
+                                                        )
+                                                    )}
+                                                    href={instanceUrl}
+                                                    target="_blank"
+                                                    rel="noreferrer"
+                                                >
+                                                    {t("go to instance")}
+                                                </a>
+                                            )}
                                         </div>
                                     </div>
                                 );
@@ -115,11 +173,7 @@ export const ReferencedInstancesTab = (props: Props) => {
                     </Accordion>
                 );
             })}
-
-            <Button className={fr.cx("fr-mt-3w")} linkProps={createInstanceLink}>
-                {t("add instance")}
-            </Button>
-        </section>
+        </>
     );
 };
 
@@ -157,11 +211,16 @@ const useStyles = tss.withName({ ReferencedInstancesTab }).create({
 
 export const { i18n } = declareComponentKeys<
     | {
-          K: "instanceCount";
-          P: { instanceCount: number; publicOrganizationCount: number };
+          K: "publicInstanceCount";
+          P: { instanceCount: number; organizationCount: number };
+      }
+    | {
+          K: "privateInstanceCount";
+          P: { instanceCount: number; organizationCount: number };
       }
     | "concerned public"
     | "go to instance"
     | "add instance"
     | "edit instance"
+    | "connect to see private instances"
 >()({ ReferencedInstancesTab });
