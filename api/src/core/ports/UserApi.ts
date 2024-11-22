9213374032501fd89577b515d60fdf2b3295fce9
Jérôme Burkard <22095555+JeromeBu@users.noreply.github.com>
2024-11-22 15:40:40 +0100

    remove organisation from keycloak, and use the one stored in agents table

--- api/src/core/ports/UserApi.ts
+++ api/src/core/ports/UserApi.ts
@@ -1,12 +1,7 @@
 export type UserApi = {
-    updateUserOrganization: (params: { userId: string; organization: string }) => Promise<void>;
     updateUserEmail: (params: { userId: string; email: string }) => Promise<void>;
     getAllowedEmailRegexp: {
         (): Promise<string>;
-        clear: () => void;
-    };
-    getAllOrganizations: {
-        (): Promise<string[]>;
         clear: () => void;
     };
     getUserCount: {
