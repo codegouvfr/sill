bcd4484a2d9ca1fe7f68aa893efb000728cfb057
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2024-12-09 16:45:07 +0100

    #215 : functions to import software from wikidata

--- api/src/core/usecases/importFromSource.ts
+++ api/src/core/usecases/importFromSource.ts
@@ -1,6 +1,7 @@
 import { DbApiV2 } from "../ports/DbApiV2";
 import { halAPIGateway } from "../adapters/hal/HalAPI";
 import { halRawSoftwareToSoftwareForm } from "../adapters/hal/getSoftwareForm";
+import { getWikidataForm } from "../adapters/wikidata/getSoftwareForm";
 
 export const importFromHALSource: (dbApi: DbApiV2) => (agentEmail: string) => Promise<Promise<number | undefined>[]> = (
     dbApi: DbApiV2
@@ -31,6 +32,43 @@ export const importFromHALSource: (dbApi: DbApiV2) => (agentEmail: string) => Pr
                 console.info("Importing HAL : ", software.docid);
                 const newSoft = await halRawSoftwareToSoftwareForm(software);
                 return dbApi.software.create({ formData: newSoft, externalDataOrigin: "HAL", agentId: agentId });
+            }
+        });
+    };
+};
+
+export const importFromWikidataSource: (
+    dbApi: DbApiV2
+) => (agentEmail: string, softwareIds: string[]) => Promise<Promise<number | undefined>[]> = (dbApi: DbApiV2) => {
+    return async (agentEmail: string, softwareIds: string[]) => {
+        const agent = await dbApi.agent.getByEmail(agentEmail);
+        const agentId = agent
+            ? agent.id
+            : await dbApi.agent.add({
+                  email: agentEmail,
+                  "isPublic": false,
+                  organization: "",
+                  about: "This is an bot user created to import data."
+              });
+
+        const dbSoftwares = await dbApi.software.getAll();
+        const dbSoftwaresNames = dbSoftwares.map(software => {
+            return software.softwareName;
+        });
+
+        return softwareIds.map(async (softwareId: string) => {
+            const newSoft = await getWikidataForm(softwareId);
+            if (!newSoft) {
+                return -1;
+            }
+
+            const index = dbSoftwaresNames.indexOf(newSoft?.softwareName ?? "");
+
+            if (index != -1) {
+                return dbSoftwares[index].softwareId;
+            } else {
+                console.log("Importing wikidata : ", softwareId);
+                return dbApi.software.create({ formData: newSoft, externalDataOrigin: "wikidata", agentId: agentId });
             }
         });
     };
