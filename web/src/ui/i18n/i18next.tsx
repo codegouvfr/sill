7ba97dd8f2d758dc048abbfe9e3340805e06935d
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2025-01-08 15:00:03 +0100

    #127 #128 add CNRS config

--- web/src/ui/i18n/i18next.tsx
+++ web/src/ui/i18n/i18next.tsx
@@ -2,22 +2,25 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import translationEn from "./sill_en.json";
 import translationFr from "./sill_fr.json";
+import cnrsEn from "./cnrs_en.json";
+import cnrsFr from "./cnrs_fr.json";
 
 export const fallbackNS = "sill";
 export const resources = {
     en: {
-        sill: translationEn
+        sill: translationEn,
+        cnrs: cnrsEn
     },
     fr: {
-        sill: translationFr
+        sill: translationFr,
+        cnrs: cnrsFr
     }
 } as const;
 
 i18next.use(initReactI18next).init({
     resources,
-    lng: "en",
-    fallbackLng: ["en", "fr"],
-    defaultNS: "sill",
+    fallbackLng: ["fr", "en"],
+    defaultNS: "cnrs",
     fallbackNS: "sill",
 
     interpolation: {
