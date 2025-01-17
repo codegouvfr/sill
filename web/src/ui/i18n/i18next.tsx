d53533f6bcabff040123b6792e87c021b81f9abc
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2025-01-17 11:18:02 +0100

    #127 : fix lint on web

--- web/src/ui/i18n/i18next.tsx
+++ web/src/ui/i18n/i18next.tsx
@@ -1,6 +1,6 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from 'i18next-browser-languagedetector';
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import translationEn from "./sill_en.json";
 import translationFr from "./sill_fr.json";
@@ -8,6 +8,7 @@ import translationFr from "./sill_fr.json";
 export const fallbackNS = "sill";
 
 // following is only for typechecking
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const _isAssignable = (fr: typeof translationFr): typeof translationEn => fr;
 
 export const resources = {
@@ -15,16 +16,19 @@ export const resources = {
     fr: { sill: translationFr }
 } as const;
 
-i18next.use(LanguageDetector).use(initReactI18next).init({
-    supportedLngs: ['en', 'fr'],
-    resources,
-    fallbackLng: ["en", "fr"],
-    defaultNS: "sill",
-    fallbackNS: "sill",
+i18next
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        supportedLngs: ["en", "fr"],
+        resources,
+        fallbackLng: ["en", "fr"],
+        defaultNS: "sill",
+        fallbackNS: "sill",
 
-    interpolation: {
-        escapeValue: false
-    }
-});
+        interpolation: {
+            escapeValue: false
+        }
+    });
 
 export default i18next;
