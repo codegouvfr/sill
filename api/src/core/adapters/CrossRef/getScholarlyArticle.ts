f964c3474e4d8b3995f7ec76033db279dfd80003
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2025-04-02 16:44:30 +0200

    see #331 : add tool to get title from DOI on crossref

---
+++ api/src/core/adapters/CrossRef/getScholarlyArticle.ts
@@ -0,0 +1,25 @@
+import { SILL } from "../../../types/SILL";
+import { crossRef } from "./api";
+
+export const getScholarlyArticle = async (doi: string): Promise<SILL.ScholarlyArticle | undefined> => {
+    const workData = await crossRef.work.get(doi).catch(error => {
+        if (error.message == "404") return undefined;
+        throw error;
+    });
+
+    if (!workData || !workData.message) {
+        return undefined;
+    }
+
+    return {
+        "@id": workData.message.DOI,
+        "@type": "ScholarlyArticle",
+        identifier: {
+            "@type": "PropertyValue",
+            "propertyID": "doi",
+            "url": new URL(`https://doi.org/${doi}`),
+            "value": workData.message.DOI
+        },
+        headline: workData.message.title[0]
+    };
+};
