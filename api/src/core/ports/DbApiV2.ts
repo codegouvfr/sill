3f3c060f774be063072568a26057863b9299d89e
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2025-04-15 13:41:54 +0200

    feat(api): move use case from repo to usercase files refs: #359

--- api/src/core/ports/DbApiV2.ts
+++ api/src/core/ports/DbApiV2.ts
@@ -1,11 +1,10 @@
-import type { Database } from "../adapters/dbApi/kysely/kysely.database";
+import type { Database, DatabaseRow } from "../adapters/dbApi/kysely/kysely.database";
 import type {
     Agent,
     Instance,
     InstanceFormData,
     ServiceProvider,
     Software,
-    SoftwareFormData,
     Source
 } from "../usecases/readWriteSillData";
 import type { OmitFromExisting } from "../utils";
@@ -20,18 +19,32 @@ type GetSoftwareFilters = {
     onlyIfUpdatedMoreThan3HoursAgo?: true;
 };
 
+// Other data, intrinsic are managed internally by the database
+export type SoftwareExtrinsicRow = Pick<
+    DatabaseRow.SoftwareRow,
+    | "name"
+    | "description"
+    | "license"
+    | "logoUrl"
+    | "versionMin"
+    | "dereferencing"
+    | "isStillInObservation"
+    | "doRespectRgaa"
+    | "isFromFrenchPublicService"
+    | "isPresentInSupportContract"
+    | "softwareType"
+    | "workshopUrls"
+    | "categories"
+    | "generalInfoMd"
+    | "keywords"
+    | "addedByAgentId"
+>;
+
+export type SoftwareExtrinsicCreation = SoftwareExtrinsicRow & Pick<DatabaseRow.SoftwareRow, "referencedSinceTime">;
+
 export interface SoftwareRepository {
-    create: (
-        params: {
-            formData: SoftwareFormData;
-        } & WithAgentId
-    ) => Promise<number>;
-    update: (
-        params: {
-            softwareSillId: number;
-            formData: SoftwareFormData;
-        } & WithAgentId
-    ) => Promise<void>;
+    create: (params: { software: SoftwareExtrinsicCreation }) => Promise<number>;
+    update: (params: { softwareId: number; software: SoftwareExtrinsicRow }) => Promise<void>;
     updateLastExtraDataFetchAt: (params: { softwareId: number }) => Promise<void>;
     getAll: (filters?: GetSoftwareFilters) => Promise<Software[]>;
     getById: (id: number) => Promise<Software | undefined>;
@@ -53,6 +66,13 @@ export interface SoftwareRepository {
 }
 
 export interface SoftwareExternalDataRepository {
+    insert: (params: { softwareId?: number; sourceSlug: string; externalId: string }) => Promise<void>;
+    update: (params: {
+        sourceSlug: string;
+        externalId: string;
+        softwareId?: number;
+        softwareExternalData: SoftwareExternalData;
+    }) => Promise<void>;
     save: (params: { softwareExternalData: SoftwareExternalData; softwareId: number | undefined }) => Promise<void>;
 }
 
@@ -124,9 +144,18 @@ export interface SourceRepository {
     getWikidataSource: () => Promise<Source | undefined>;
 }
 
+export interface SimilarSoftwareRepository {
+    insert: (params: {
+        softwareId: number;
+        externalIds: { sourceSlug: string; externalId: string }[];
+    }) => Promise<void>;
+    getById: (params: { softwareId: number }) => Promise<{ sourceSlug: string; externalId: string }[]>;
+}
+
 export type DbApiV2 = {
     source: SourceRepository;
     software: SoftwareRepository;
+    similarSoftware: SimilarSoftwareRepository;
     softwareExternalData: SoftwareExternalDataRepository;
     otherSoftwareExtraData: OtherSoftwareExtraDataRepository;
     instance: InstanceRepository;
