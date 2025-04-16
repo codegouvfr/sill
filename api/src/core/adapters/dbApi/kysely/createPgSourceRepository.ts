e84a0b23cb541b84911eb22e099485d81c114f99
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2025-04-16 15:08:50 +0200

    fix(api): remove null from type source refs: #359

--- api/src/core/adapters/dbApi/kysely/createPgSourceRepository.ts
+++ api/src/core/adapters/dbApi/kysely/createPgSourceRepository.ts
@@ -1,10 +1,16 @@
 import { Kysely } from "kysely";
 import { SourceRepository } from "../../../ports/DbApiV2";
 import { Database } from "./kysely.database";
+import { stripNullOrUndefinedValues } from "./kysely.utils";
 
 export const createPgSourceRepository = (db: Kysely<Database>): SourceRepository => ({
     getMainSource: async () =>
-        db.selectFrom("sources").selectAll().orderBy("priority", "desc").executeTakeFirstOrThrow(),
+        db
+            .selectFrom("sources")
+            .selectAll()
+            .orderBy("priority", "desc")
+            .executeTakeFirstOrThrow()
+            .then(row => stripNullOrUndefinedValues(row)),
     getWikidataSource: async () =>
         db
             .selectFrom("sources")
@@ -12,4 +18,5 @@ export const createPgSourceRepository = (db: Kysely<Database>): SourceRepository
             .where("kind", "=", "wikidata")
             .orderBy("priority", "desc")
             .executeTakeFirstOrThrow()
+            .then(row => stripNullOrUndefinedValues(row))
 });
