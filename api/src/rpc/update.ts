75590c81e00173193b16c2364225e512eccb5122
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2025-04-16 15:28:31 +0200

    feat(api): create usecase refreshExternalData refs: #359

--- api/src/rpc/update.ts
+++ api/src/rpc/update.ts
@@ -3,22 +3,33 @@ import type { Equals } from "tsafe";
 import { assert } from "tsafe/assert";
 import { Database } from "../core/adapters/dbApi/kysely/kysely.database";
 import { createPgDialect } from "../core/adapters/dbApi/kysely/kysely.dialect";
-import type { ExternalDataOrigin } from "../core/ports/GetSoftwareExternalData";
-import { updateTool } from "../core/updateTools";
+import { refreshExternalData } from "../core/usecases/refreshExternalData";
+import { createKyselyPgDbApi } from "../core/adapters/dbApi/kysely/createPgDbApi";
+import { DbApiV2 } from "../core/ports/DbApiV2";
+
+type PgDbConfig = { dbKind: "kysely"; kyselyDb: Kysely<Database> };
+
+type DbConfig = PgDbConfig;
+
+const getDbApiAndInitializeCache = (dbConfig: DbConfig): { dbApi: DbApiV2 } => {
+    if (dbConfig.dbKind === "kysely") {
+        return {
+            dbApi: createKyselyPgDbApi(dbConfig.kyselyDb)
+        };
+    }
+
+    const shouldNotBeReached: never = dbConfig.dbKind;
+    throw new Error(`Unsupported case: ${shouldNotBeReached}`);
+};
 
 export async function startUpdateService(params: {
     githubPersonalAccessTokenForApiRateLimit: string;
     isDevEnvironnement: boolean;
-    externalSoftwareDataOrigin: ExternalDataOrigin;
     databaseUrl: string;
 }) {
-    const {
-        githubPersonalAccessTokenForApiRateLimit,
-        isDevEnvironnement,
-        externalSoftwareDataOrigin,
-        databaseUrl,
-        ...rest
-    } = params;
+    console.log("Starting fetching of external data on remote sources");
+    console.time("Starting fetching of external data on remote sources: Done");
+    const { githubPersonalAccessTokenForApiRateLimit, isDevEnvironnement, databaseUrl, ...rest } = params;
 
     assert<Equals<typeof rest, {}>>();
 
@@ -26,14 +37,16 @@ export async function startUpdateService(params: {
 
     const kyselyDb = new Kysely<Database>({ dialect: createPgDialect(databaseUrl) });
 
-    const result = await updateTool({
-        "dbConfig": {
-            "dbKind": "kysely",
-            "kyselyDb": kyselyDb
-        },
-        githubPersonalAccessTokenForApiRateLimit,
-        "externalSoftwareDataOrigin": externalSoftwareDataOrigin
+    const { dbApi } = getDbApiAndInitializeCache({
+        "dbKind": "kysely",
+        "kyselyDb": kyselyDb
     });
 
-    console.log("Update sucessfull", result);
+    await refreshExternalData({
+        dbApi,
+        githubPersonalAccessTokenForApiRateLimit,
+        skipSince: 180
+    });
+
+    console.timeEnd("Starting fetching of external data on remote sources: Done");
 }
