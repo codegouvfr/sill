49152b767daf6b8b44ffd0230295e566cb039621
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2025-04-22 17:27:41 +0200

    refactor(ui): change identification object structure refs: #296

--- web/src/ui/pages/softwareDetails/HeaderDetailCard.tsx
+++ web/src/ui/pages/softwareDetails/HeaderDetailCard.tsx
@@ -24,7 +24,7 @@ export type Props = {
               lastRecommendedVersion?: string;
           }
         | undefined;
-    authors: Array<ApiTypes.SILL.Person | ApiTypes.SILL.Organization>;
+    authors: Array<ApiTypes.Person | ApiTypes.Organization>;
     officialWebsite?: string;
     documentationWebsite?: string;
     sourceCodeRepository?: string;
