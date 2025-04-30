57ba47e558279cf07112851430c0a3a1d8f349fc
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2025-04-30 13:09:04 +0200

    fix(api): for multi source start and update refs: #361

--- api/src/rpc/createTestCaller.ts
+++ api/src/rpc/createTestCaller.ts
@@ -2,9 +2,6 @@ import { Kysely } from "kysely";
 import { bootstrapCore } from "../core";
 import { Database } from "../core/adapters/dbApi/kysely/kysely.database";
 import { createPgDialect } from "../core/adapters/dbApi/kysely/kysely.dialect";
-import { getWikidataSoftware } from "../core/adapters/wikidata/getWikidataSoftware";
-import { getWikidataSoftwareOptions } from "../core/adapters/wikidata/getWikidataSoftwareOptions";
-import { ExternalDataOrigin } from "../core/ports/GetSoftwareExternalData";
 import { testPgUrl } from "../tools/test.helpers";
 import { createRouter } from "./router";
 import { User } from "./user";
@@ -21,12 +18,10 @@ export const defaultUser: User = {
 export type ApiCaller = Awaited<ReturnType<typeof createTestCaller>>["apiCaller"];
 
 export const createTestCaller = async ({ user }: TestCallerConfig = { user: defaultUser }) => {
-    const externalSoftwareDataOrigin: ExternalDataOrigin = "wikidata";
     const kyselyDb = new Kysely<Database>({ dialect: createPgDialect(testPgUrl) });
 
     const { dbApi, useCases, uiConfig } = await bootstrapCore({
-        "dbConfig": { dbKind: "kysely", kyselyDb },
-        "externalSoftwareDataOrigin": externalSoftwareDataOrigin
+        "dbConfig": { dbKind: "kysely", kyselyDb }
     });
 
     const { router } = createRouter({
@@ -34,9 +29,6 @@ export const createTestCaller = async ({ user }: TestCallerConfig = { user: defa
         dbApi,
         oidcParams: { issuerUri: "http://fake.url", clientId: "fake-client-id" },
         redirectUrl: undefined,
-        externalSoftwareDataOrigin,
-        getSoftwareExternalDataOptions: getWikidataSoftwareOptions,
-        getSoftwareExternalData: getWikidataSoftware,
         uiConfig
     });
 
