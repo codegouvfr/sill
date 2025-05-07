0a98f11399a307df4e5cb5861d468650eedc379c
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2025-05-07 08:55:05 +0200

    feat: migrate db

--- api/src/core/adapters/dbApi/kysely/migrations/1745402275178-add_new_source_kind.ts
+++ api/src/core/adapters/dbApi/kysely/migrations/1745402275178-add_new_source_kind.ts
@@ -15,6 +15,36 @@ export async function up(db: Kysely<any>): Promise<void> {
             col.setDataType(sql`external_data_origin_type USING kind::external_data_origin_type`)
         )
         .execute();
+
+    const compiledSoftwarePackages = await db
+        .selectFrom("compiled_softwares")
+        .select(["comptoirDuLibreSoftware", "softwareId"])
+        .execute();
+
+    if (compiledSoftwarePackages && compiledSoftwarePackages.length > 0) {
+        // Create Comptoir Du Libre
+        const cDLSource = {
+            slug: "comptoirDuLibre",
+            kind: "ComptoirDuLibre",
+            url: "https://comptoir-du-libre.org/",
+            priority: 2
+        };
+        await db.insertInto("sources").values(cDLSource).executeTakeFirst();
+
+        const externalDataIds = compiledSoftwarePackages.map(row => ({
+            externalId: row.comptoirDuLibreSoftware.id,
+            sourceSlug: cDLSource.slug,
+            softwareId: row.softwareId,
+            developers: JSON.stringify([]),
+            label: JSON.stringify({}),
+            description: JSON.stringify({})
+        }));
+
+        await db.insertInto("software_external_datas").values(externalDataIds).execute();
+    }
+
+    // Delete row
+    await db.schema.alterTable("compiled_softwares").dropColumn("comptoirDuLibreSoftware").execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
