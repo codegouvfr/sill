2976e302f46efb095b669c1b0ed584f6632c625f
Jérôme Burkard <22095555+JeromeBu@users.noreply.github.com>
2025-05-07 11:52:56 +0200

    fix: fixes for review

--- api/src/core/utils.ts
+++ api/src/core/utils.ts
@@ -3,25 +3,3 @@
 // SPDX-License-Identifier: MIT
 
 export type OmitFromExisting<T, K extends keyof T> = Omit<T, K>;
-
-type BuildIndex<T> = (params: { arrayOfObject: T[]; fieldObject: keyof T }) => Record<string, T>;
-
-export const buildIndex: BuildIndex<any> = <T>({
-    arrayOfObject,
-    fieldObject
-}: {
-    arrayOfObject: T[];
-    fieldObject: keyof T;
-}): Record<string, T> => {
-    if (arrayOfObject && arrayOfObject.length > 0 && typeof arrayOfObject[0][fieldObject] === "string") {
-        return arrayOfObject.reduce(
-            (acc, item) => {
-                const key = item[fieldObject] as string;
-                acc[key] = item;
-                return acc;
-            },
-            {} as Record<string, T>
-        );
-    }
-    return {};
-};
