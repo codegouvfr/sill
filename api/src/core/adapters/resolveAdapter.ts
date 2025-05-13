2532aa34223392a432f98ed42c7ff03ac211209d
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2025-05-13 17:28:43 +0200

    feat(api): add a ComptoirDuLibre source gateway refs: #295

--- api/src/core/adapters/resolveAdapter.ts
+++ api/src/core/adapters/resolveAdapter.ts
@@ -2,6 +2,7 @@ import { SourceGateway } from "../ports/SourceGateway";
 import { DatabaseDataType } from "../ports/DbApiV2";
 import { halSourceGateway } from "./hal";
 import { wikidataSourceGateway } from "./wikidata";
+import { comptoirDuLibreSourceGateway } from "./comptoirDuLibre";
 
 export const resolveAdapterFromSource = (source: DatabaseDataType.SourceRow): SourceGateway => {
     switch (source.kind) {
@@ -9,6 +10,8 @@ export const resolveAdapterFromSource = (source: DatabaseDataType.SourceRow): So
             return halSourceGateway;
         case "wikidata":
             return wikidataSourceGateway;
+        case "ComptoirDuLibre":
+            return comptoirDuLibreSourceGateway;
         default:
             const unreachableCase: never = source.kind;
             throw new Error(`Unreachable case: ${unreachableCase}`);
