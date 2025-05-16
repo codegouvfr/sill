8e4277cacc1404da6183a485e7332f6e5b01f1a4
Jérôme Burkard <22095555+JeromeBu@users.noreply.github.com>
2025-05-16 16:32:25 +0200

    fix: oidc import

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -4,7 +4,7 @@ import { MuiDsfrThemeProvider } from "@codegouvfr/react-dsfr/mui";
 import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
 import { assert } from "tsafe/assert";
 import "./ui/i18n/i18next";
-import { handleOidcCallback } from "oidc-spa/oidc/handleOidcCallback";
+import { handleOidcCallback } from "oidc-spa/core/handleOidcCallback";
 
 handleOidcCallback();
 
