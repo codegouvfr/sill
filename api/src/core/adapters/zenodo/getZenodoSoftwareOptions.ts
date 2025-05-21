612fb505e34ef8115d1b93289adcd53736d78720
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2025-05-21 11:22:17 +0200

    feat: manage more data, import from Zenodo and add test refs: #204

--- api/src/core/adapters/zenodo/getZenodoSoftwareOptions.ts
+++ api/src/core/adapters/zenodo/getZenodoSoftwareOptions.ts
@@ -10,16 +10,16 @@ export const getZenodoSoftwareOptions: GetSoftwareExternalDataOptions = async ({
     const zenodoApi = makeZenodoApi();
     const records = await zenodoApi.records.getByNameAndType(queryString, "software");
 
-    if (!records || records.length === 0) return [];
+    if (!records.hits || records.hits.hits.length === 0) return [];
 
-    return records.map(record => formatRecordToSoftwareOption(record, source));
+    return records.hits.hits.map(record => formatRecordToSoftwareOption(record, source));
 };
 
 function formatRecordToSoftwareOption(record: Zenodo.Record, source: Source): SoftwareExternalDataOption {
     return {
         externalId: record.id.toString(),
         label: record.title,
-        description: record.metadata.description,
+        description: record.metadata.description ?? "",
         isLibreSoftware: record.metadata.access_right === "open", // Not sure
         sourceSlug: source.slug
     };
