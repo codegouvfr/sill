f54302a74792576c690a60635b9ec38fb7a443c4
Jérôme Burkard <22095555+JeromeBu@users.noreply.github.com>
2025-05-28 17:50:06 +0200

    feat: setting up seed script

---
+++ api/scripts/seed.ts
@@ -0,0 +1,222 @@
+// the following script is used to seed the database with initial data
+
+import { Kysely } from "kysely";
+import { createKyselyPgDbApi } from "../src/core/adapters/dbApi/kysely/createPgDbApi";
+import { DbAgent, DbApiV2 } from "../src/core/ports/DbApiV2";
+import { Database } from "../src/core/adapters/dbApi/kysely/kysely.database";
+import { createPgDialect } from "../src/core/adapters/dbApi/kysely/kysely.dialect";
+import { SoftwareFormData, Source } from "../src/lib/ApiTypes";
+import { OmitFromExisting } from "../src/core/utils";
+
+const seed = async () => {
+    const dbUrl = process.env.DATABASE_URL;
+
+    if (!dbUrl) throw new Error("DATABASE_URL environment variable is not set");
+
+    const db = new Kysely<Database>({ dialect: createPgDialect(dbUrl) });
+
+    const dbApi: DbApiV2 = createKyselyPgDbApi(db);
+
+    // Clear existing data
+    console.info("Clearing existing data");
+
+    await db.deleteFrom("software_external_datas").execute();
+    await db.deleteFrom("softwares__similar_software_external_datas").execute();
+    await db.deleteFrom("softwares").execute();
+    await db.deleteFrom("agents").execute();
+    await db.deleteFrom("sources").execute();
+
+    console.info("Data cleared");
+
+    console.info("Adding source");
+    const source = {
+        slug: "wikidata",
+        description: null,
+        url: "https://www.wikidata.org/",
+        kind: "wikidata",
+        priority: 1
+    } satisfies Source;
+    await db.insertInto("sources").values(source).execute();
+
+    const someAgent: OmitFromExisting<DbAgent, "id"> = {
+        email: "some@agent.com",
+        about: "This is a fake agent for seeding purposes.",
+        isPublic: true,
+        organization: "Seed Organization"
+    };
+
+    console.info("Adding agent");
+    const agentId = await dbApi.agent.add(someAgent);
+
+    console.info("Adding software packages");
+    const softwarePackagesFormData: SoftwareFormData[] = [
+        {
+            softwareName: "React",
+            softwareDescription: "A JavaScript library for building user interfaces.",
+            softwareType: {
+                type: "stack"
+            },
+            externalIdForSource: undefined,
+            sourceSlug: "wikidata",
+            comptoirDuLibreId: undefined,
+            softwareLicense: "MIT",
+            softwareMinimalVersion: "18.0.0",
+            similarSoftwareExternalDataIds: [],
+            softwareLogoUrl: "https://react.dev/favicon.ico",
+            softwareKeywords: ["javascript", "ui", "frontend", "library"],
+            isPresentInSupportContract: false,
+            isFromFrenchPublicService: false,
+            doRespectRgaa: null
+        },
+        {
+            softwareName: "Git",
+            softwareDescription: "A free and open source distributed version control system.",
+            softwareType: {
+                type: "desktop/mobile",
+                os: { mac: true, windows: true, linux: true, android: false, ios: false }
+            },
+            externalIdForSource: undefined,
+            sourceSlug: "wikidata",
+            comptoirDuLibreId: undefined,
+            softwareLicense: "GPL-2.0",
+            softwareMinimalVersion: "2.0.0",
+            similarSoftwareExternalDataIds: [],
+            softwareLogoUrl: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+            softwareKeywords: ["vcs", "version control", "git", "scm"],
+            isPresentInSupportContract: false,
+            isFromFrenchPublicService: false,
+            doRespectRgaa: null
+        },
+        {
+            softwareName: "OpenOffice",
+            softwareDescription:
+                "An open-source office software suite for word processing, spreadsheets, presentations, graphics, databases and more.",
+            softwareType: {
+                type: "desktop/mobile",
+                os: { mac: true, windows: true, linux: true, android: false, ios: false }
+            },
+            externalIdForSource: undefined,
+            sourceSlug: "wikidata",
+            comptoirDuLibreId: undefined,
+            softwareLicense: "Apache-2.0",
+            softwareMinimalVersion: "4.1.0",
+            similarSoftwareExternalDataIds: [],
+            softwareLogoUrl: "https://www.openoffice.org/images/AOO_logos/AOO_Logo_FullColor.svg",
+            softwareKeywords: ["office", "suite", "word", "spreadsheet", "presentation"],
+            isPresentInSupportContract: false,
+            isFromFrenchPublicService: false,
+            doRespectRgaa: null
+        },
+        {
+            softwareName: "VLC media player",
+            softwareDescription: "A free and open source cross-platform multimedia player and framework.",
+            softwareType: {
+                type: "desktop/mobile",
+                os: { mac: true, windows: true, linux: true, android: true, ios: true }
+            },
+            externalIdForSource: undefined,
+            sourceSlug: "wikidata",
+            comptoirDuLibreId: undefined,
+            softwareLicense: "GPL-2.0",
+            softwareMinimalVersion: "3.0.0",
+            similarSoftwareExternalDataIds: [],
+            softwareLogoUrl: "https://www.videolan.org/images/favicon.png",
+            softwareKeywords: ["media", "player", "video", "audio", "vlc"],
+            isPresentInSupportContract: false,
+            isFromFrenchPublicService: false,
+            doRespectRgaa: null
+        },
+        {
+            softwareName: "GIMP",
+            softwareDescription: "GNU Image Manipulation Program: a free and open source image editor.",
+            softwareType: {
+                type: "desktop/mobile",
+                os: { mac: true, windows: true, linux: true, android: false, ios: false }
+            },
+            externalIdForSource: undefined,
+            sourceSlug: "wikidata",
+            comptoirDuLibreId: undefined,
+            softwareLicense: "GPL-3.0",
+            softwareMinimalVersion: "2.10.0",
+            similarSoftwareExternalDataIds: [],
+            softwareLogoUrl: "https://www.gimp.org/images/wilber-big.png",
+            softwareKeywords: ["image", "editor", "graphics", "gimp"],
+            isPresentInSupportContract: false,
+            isFromFrenchPublicService: false,
+            doRespectRgaa: null
+        },
+        {
+            softwareName: "Onyxia",
+            softwareDescription:
+                "Application web pour simplifier la configuration d'environnement datascience sur Kubernetes.",
+            softwareType: {
+                type: "cloud"
+            },
+            externalIdForSource: "Q110492908",
+            sourceSlug: "wikidata",
+            comptoirDuLibreId: 461,
+            softwareLicense: "MIT",
+            softwareMinimalVersion: "0.26.25",
+            similarSoftwareExternalDataIds: [],
+            softwareLogoUrl:
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Onyxia.svg/250px-Onyxia.svg.png",
+            softwareKeywords: ["hébergement", "hosting", "plateforme", "platform", "cloud", "nuage"],
+            isPresentInSupportContract: false,
+            isFromFrenchPublicService: true,
+            doRespectRgaa: false
+        }
+    ];
+
+    for (const formData of softwarePackagesFormData) {
+        await dbApi.software.create({ agentId, formData });
+    }
+
+    // Add instances for Onyxia
+    console.info("Adding instances for Onyxia");
+
+    // Get the Onyxia software ID
+    const onyxiaSoftware = await db
+        .selectFrom("softwares")
+        .select("id")
+        .where("name", "=", "Onyxia")
+        .executeTakeFirstOrThrow();
+
+    const onyxiaInstances = [
+        {
+            mainSoftwareSillId: onyxiaSoftware.id,
+            organization: "DINUM",
+            targetAudience: "Agents de l'État",
+            instanceUrl: "https://onyxia-demo.data.gouv.fr",
+            isPublic: true
+        },
+        {
+            mainSoftwareSillId: onyxiaSoftware.id,
+            organization: "Ministère de l'Intérieur",
+            targetAudience: "Services de police et de gendarmerie",
+            instanceUrl: "https://onyxia.interieur.gouv.fr",
+            isPublic: false
+        },
+        {
+            mainSoftwareSillId: onyxiaSoftware.id,
+            organization: "Ministère de l'Éducation Nationale",
+            targetAudience: "Enseignants et chercheurs",
+            instanceUrl: "https://onyxia.education.gouv.fr",
+            isPublic: true
+        }
+    ];
+
+    for (const instanceData of onyxiaInstances) {
+        await dbApi.instance.create({ agentId, formData: instanceData });
+    }
+};
+
+seed()
+    .then(() => {
+        console.log("Database seeded successfully");
+        process.exit(0);
+    })
+    .catch(error => {
+        console.error("Error seeding database:", error.message);
+        console.error(error);
+        process.exit(1);
+    });
--- docs/2-getting-started.md
+++ docs/2-getting-started.md
@@ -3,15 +3,15 @@
 ## Run in local
 
 To run the application in local, you need :
+
 - docker and docker-compose, do setup the database, and local keycloak server.
 - nodejs 22 and yarn to run the web application (front and back)
-
 
 1. Clone the repository
 
 ```bash
 git clone git@github.com:codegouvfr/catalogi.git
-```   
+```
 
 2. Install the dependencies
 
@@ -34,14 +34,20 @@ Than you should adjust the variables in the `.env`, look at [the different varia
 docker compose -f docker-compose.resources.yml up --build -d
 ```
 
-5. start the frontend and backend in dev
+5. load the database with some data by running seed
+
+```bash
+cd api && yarn db:seed
+```
+
+6. start the frontend and backend in dev
 
 ```bash
 yarn dev # from the root, this will run both the frontend and backend `yarn dev`
 ```
 
+The docker-compose.resources.yml will start:
 
-The docker-compose.resources.yml will start: 
 - a postgres database
 - an adminer instance so that you can access the database with a UI (http://localhost:8082)
 - a keycloak server (http://localhost:8080/) in dev mode, with a default admin user `admin` and password `admin`.
@@ -50,9 +56,10 @@ The docker-compose.resources.yml will start:
 
 When you want to push a new release, you need to update the version number in the root `package.json`.
 The CI will :
+
 - validate the code
 - create a release on GitHub
-- create the corresponding docker images and push them on docker hub : 
+- create the corresponding docker images and push them on docker hub :
   - [codegouvfr/catalogi-web](https://hub.docker.com/r/codegouvfr/catalogi-web/tags)
   - [codegouvfr/catalogi-api](https://hub.docker.com/r/codegouvfr/catalogi-api/tags)
 
@@ -63,15 +70,14 @@ You can deploy Catalogi with the following methods:
 - [Docker Compose](4-deploying-with-docker-compose.md)
 - [Kubernetes (with Helm charts)](5-deploying-with-kubernetes.md)
 
-
 ## Source code
 
 All the source code is hosted on this repository.
 
 The repository is the source code for 3 differents apps :
 
-* `/web`: The web application, runs in the browser. The site is here : [https://code.gouv.fr/catalogi](https://code.gouv.fr/catalogi)
-* `/api`: The RPC API consumed by the web application.
-* `/docs`: The documentation. It is hosted on [https://codegouvfr.github.io/catalogi](https://codegouvfr.github.io/catalogi)
+- `/web`: The web application, runs in the browser. The site is here : [https://code.gouv.fr/catalogi](https://code.gouv.fr/catalogi)
+- `/api`: The RPC API consumed by the web application.
+- `/docs`: The documentation. It is hosted on [https://codegouvfr.github.io/catalogi](https://codegouvfr.github.io/catalogi)
 
 The data is held in a PostgreSQL database.
