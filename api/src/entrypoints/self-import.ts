46cae68061e2b34dda3dfe604e36e0de37d43a80
Guillaume Bourdat <guillaume.bourdat@univ-grenoble-alpes.fr>
2025-06-05 15:48:59 +0200

    refactor: change name to import-from-inner-identifiers refs: 383

--- api/src/entrypoints/self-import.ts
+++
@@ -1,6 +0,0 @@
-import { env } from "../env";
-import { startSelfImportService } from "../rpc/self-import";
-
-startSelfImportService(env).then(() =>
-    console.info("[Entrypoint:Self-Import] Import sucessuful ✅ Closing self import")
-);
