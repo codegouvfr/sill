f4b5c451c7d30ef6c23e305f132710b0a211e1fa
Jérôme Burkard <22095555+JeromeBu@users.noreply.github.com>
2025-06-06 15:14:54 +0200

    feat: create script to sync helm charts with root package.json version

---
+++ scripts/sync-helm-app-version.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const path = require("path");
+const yaml = require("js-yaml");
+
+const rootDir = path.join(__dirname, "..");
+const rootPackageJsonPath = path.join(rootDir, "package.json");
+const chartYamlPath = path.join(
+	rootDir,
+	"helm-charts",
+	"catalogi",
+	"Chart.yaml",
+);
+
+try {
+	const packageJsonContent = fs.readFileSync(rootPackageJsonPath, "utf8");
+	const { version: appVersion } = JSON.parse(packageJsonContent);
+
+	if (!appVersion) {
+		throw new Error("Version not found in root package.json");
+	}
+
+	const chartYamlContent = fs.readFileSync(chartYamlPath, "utf8");
+	const chartYaml = yaml.load(chartYamlContent);
+
+	if (chartYaml.appVersion !== appVersion) {
+		console.log(
+			`Updating Helm chart appVersion from ${chartYaml.appVersion} to ${appVersion}...`,
+		);
+		chartYaml.appVersion = appVersion;
+		const newChartYamlContent = yaml.dump(chartYaml);
+		fs.writeFileSync(chartYamlPath, newChartYamlContent, "utf8");
+		console.log("✅ Helm chart appVersion updated");
+	} else {
+		console.log("✅ Helm chart appVersion is already in sync.");
+	}
+} catch (error) {
+	console.error("❌ Error syncing Helm chart version:", error.message);
+	process.exit(1);
+}
