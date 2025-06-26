338af0b0f38759386d27d94b013c96e6edff756d
Jérôme Burkard <22095555+JeromeBu@users.noreply.github.com>
2025-06-26 15:54:31 +0200

    feat: add knip to spot dead code, remove lots of unused tools

--- api/src/tools/keycloakAdminApiClient.ts
+++
@@ -1,7 +0,0 @@
-export type User = {
-    id: string;
-    email: string;
-    createdTimestamp: number;
-    attributes: Record<string, string[]>;
-    emailVerified: boolean;
-};
--- api/src/tools/octokit-addons/index.ts
+++
@@ -1,4 +0,0 @@
-export { getCommitAheadFactory as getChangeLogFactory } from "./getCommitAhead";
-export { Commit, getCommitAsyncIterableFactory } from "./getCommitAsyncIterable";
-export { getCommonOriginFactory } from "./getCommonOrigin";
-export { listCommitFactory } from "./listCommit";
